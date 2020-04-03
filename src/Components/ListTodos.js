29b19d99e5e6fb280dc3b95d3a1efd88489d6d57
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-04-03 15:13:44 -0600

    Add table layout to show todos

--- src/Components/ListTodos.js
+++ src/Components/ListTodos.js
@@ -4,7 +4,24 @@ const ListTodos = () => {
   
     return (
         <section>
-            
+          <table className="table mt-5 text-center">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Description</th>
+                <th scope="col">Edit</th>
+                <th scope="col">Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                {/* <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                </tr> */}
+            </tbody>
+          </table>
         </section>
     )
 };
