5dd174a9c72f7756e203dda5f241fa877a268b59
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-04-02 22:54:42 -0600

    Add hooks to inputtodo to set state of input

--- src/Components/InputTodo.js
+++ src/Components/InputTodo.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const InputTodo = () => {
+
+   const [description, setDescription] = useState('');
+
     return (
-        <section>
+        <section className='text-center mt-5'>
             <h2>PERN Todo List</h2>
-            <form>
-                <input type='text' />
-                <button>Add</button>
+            <form className='d-flex mt-5'>
+                <input type='text' 
+                       className='form-control' 
+                       value={description} 
+                       onChange = {(e) => setDescription(e.target.value)}
+                />
+                <button className='btn btn-success' >Add</button>
             </form>
         </section>
     )
