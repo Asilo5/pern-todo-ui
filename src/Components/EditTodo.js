c9d5d880acdbe1910eca12f7496437f946b11113
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-04-06 20:23:54 -0600

    Add EditTodos component basic layout

---
+++ src/Components/EditTodo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EditTodos = () => {
+  return (
+      <section className='edit-todos'>
+         <h2>Edit</h2>
+      </section>
+  )
+}
+
+export default EditTodos;
