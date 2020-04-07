694517e57dd0c4ded1fd723b13e8b0cc501b7f8d
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-04-06 20:34:05 -0600

    Add basic bootstrap modal layout

--- src/Components/EditTodo.js
+++ src/Components/EditTodo.js
@@ -2,8 +2,26 @@ import React from 'react';
 
 const EditTodos = () => {
   return (
-      <section className='edit-todos'>
-         <h2>Edit</h2>
+      <section>
+        <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#exampleModal"> Edit </button>
+
+        <section class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <section class="modal-dialog" role="document">
+            <section class="modal-content">
+                <section class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                </section>
+                <section class="modal-body">
+                    ...
+                </section>
+                <section class="modal-footer">
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </section>
+            </section>
+        </section>
+        </section>
       </section>
   )
 }
