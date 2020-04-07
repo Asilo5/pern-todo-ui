b925b56706413027f94555e8dc519bee3e845ef0
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-04-06 20:28:31 -0600

    Add EditTodos into the mapped table

--- src/Components/ListTodos.js
+++ src/Components/ListTodos.js
@@ -45,7 +45,7 @@ const ListTodos = () => {
                     return <tr key={todo.todo_id}>
                     <th scope="row">{todo.todo_id}</th>
                     <td>{todo.description}</td>
-                    <td><button className='btn btn-warning'>Edit</button></td>
+                    <td><EditTodos/></td>
                     <td><button className='btn btn-danger' onClick={() => deleteTodo(todo.todo_id)}>Delete</button></td>
                     </tr> 
                 })}
